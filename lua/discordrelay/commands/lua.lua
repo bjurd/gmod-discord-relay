ba6121116f97c0eb915020736bd30114aa448b5f
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-03-16 05:16:30 -0400

    Make lua command give outputs

--- lua/discordrelay/commands/lua.lua
+++ lua/discordrelay/commands/lua.lua
@@ -2,5 +2,74 @@ DiscordRelay.Commands.RegisterCommand("lua", DiscordRelay.Enums.CommandPermissio
 	local Lua = table.concat(Arguments, " ")
 	if not isstring(Lua) or string.len(Lua) < 1 then return end
 
-	RunString(Lua, "DiscordRelay")
+	local LuaFn = CompileString(Lua, "DiscordRelay", false)
+
+	if not isfunction(LuaFn) then
+		DiscordRelay.Util.WebhookAutoSend({
+			["username"] = "Lua Error",
+			["embeds"] = {
+				DiscordRelay.Util.CreateEmbed(
+					Color(255, 0, 0),
+					"Lua Compilation Error",
+					LuaFn
+				)
+			}
+		})
+
+		return
+	end
+
+	local Result, A, B, C, D, E, F = pcall(LuaFn)
+
+	if Result ~= true then
+		DiscordRelay.Util.WebhookAutoSend({
+			["username"] = "Lua Error",
+			["embeds"] = {
+				DiscordRelay.Util.CreateEmbed(
+					Color(255, 0, 0),
+					"Lua Runtime Error",
+					A
+				)
+			}
+		})
+
+		return
+	end
+
+	if A ~= nil then
+		local Returns = {
+			A ~= nil and tostring(A) or nil,
+			B ~= nil and tostring(B) or nil,
+			C ~= nil and tostring(C) or nil,
+			D ~= nil and tostring(D) or nil,
+			E ~= nil and tostring(E) or nil,
+			F ~= nil and tostring(F) or nil
+		}
+
+		Returns = table.concat(Returns, ", ")
+
+		DiscordRelay.Util.WebhookAutoSend({
+			["username"] = "Lua Results",
+			["embeds"] = {
+				DiscordRelay.Util.CreateEmbed(
+					Color(0, 255, 0),
+					"Lua Runtime Results",
+
+					Format("```\n%s\n```", DiscordRelay.Util.MarkdownEscape(Returns))
+				)
+			}
+		})
+	else
+		DiscordRelay.Util.WebhookAutoSend({
+			["username"] = "Lua Results",
+			["embeds"] = {
+				DiscordRelay.Util.CreateEmbed(
+					Color(0, 255, 0),
+					"Ran Lua on Server",
+
+					Format("```\n%s\n```", DiscordRelay.Util.MarkdownEscape(Lua))
+				)
+			}
+		})
+	end
 end)
