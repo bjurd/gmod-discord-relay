2227e22f1f7fc2399a9b9531c3a966dc9d9f90c1
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-03-16 00:37:59 -0400

    Add "Server is now online" startup message

--- lua/discordrelay/events/ready.lua
+++ lua/discordrelay/events/ready.lua
@@ -2,4 +2,11 @@ hook.Add("DiscordRelay::DispatchEvent", "DiscordRelay::Ready", function(Type, Me
 	if Type ~= "READY" then return end
 
 	DiscordRelay.Socket.SessionID = Message.session_id
+
+	DiscordRelay.Util.WebhookAutoSend({
+		["username"] = "Server Status",
+		["embeds"] = {
+			DiscordRelay.Util.CreateEmbed(Color(0, 255, 0), GetHostName(), "Server is now online!")
+		}
+	})
 end)
