3dcfee4a54439fb7a753d9a1e847c055ac85ca42
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-03-16 00:53:43 -0400

    Speedup screenshot command
    Lower default quality and split attempts across frames for less lag

--- lua/discordrelay/client/screenshot.lua
+++ lua/discordrelay/client/screenshot.lua
@@ -1,35 +1,33 @@
 net.Receive("DiscordRelay::Screenshot", function()
+	local Quality = 60
+
 	hook.Add("PostRender", "DiscordRelay::Screenshot", function()
+		if gui.IsGameUIVisible() then return end
+
 		local CaptureData = {
 			["format"] = "jpeg",
 			["x"] = 0,
 			["y"] = 0,
 			["w"] = ScrW(),
-			["h"] = ScrH()
+			["h"] = ScrH(),
+			["quality"] = Quality
 		}
 
-		local function Capture(Quality)
+		local Data = render.Capture(CaptureData)
+		if not isstring(Data) then return end
+
+		Data = util.Compress(Data)
+		local Length = string.len(Data)
+
+		if Length > 64 * 1024 then
+			Quality = Quality - 10
+
 			if Quality < 1 then -- Can't get a good capture
-				return false
+				hook.Remove("PostRender", "DiscordRelay::Screenshot")
 			end
 
-			CaptureData.quality = Quality
-
-			local Data = render.Capture(CaptureData)
-			if not isstring(Data) then return false end
-
-			Data = util.Compress(Data)
-			local Length = string.len(Data)
-
-			if Length > 65 * 1024 then
-				return Capture(Quality - 10)
-			else
-				return Data, Length
-			end
+			return
 		end
-
-		local Data, Length = Capture(80)
-		if not Data then return end
 
 		hook.Remove("PostRender", "DiscordRelay::Screenshot")
 
