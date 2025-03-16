4592a0eca12189ebba4bd76d9f7ebb7ea11801e5
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-03-16 16:35:02 -0400

    Add prefix to Discord messages
    Blurple

--- lua/discordrelay/client/discordmsg.lua
+++ lua/discordrelay/client/discordmsg.lua
@@ -1,7 +1,9 @@
+local PrefixColor = Color(114, 137, 218, 255)
+
 net.Receive("DiscordRelay::Message", function()
 	local Color = net.ReadColor(false)
 	local Username = net.ReadString()
 	local Content = net.ReadString()
 
-	chat.AddText(Color, Username, ": ", color_white, Content)
+	chat.AddText(PrefixColor, "[Discord] ", Color, Username, ": ", color_white, Content)
 end)
