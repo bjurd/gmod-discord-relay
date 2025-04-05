5ad2b61ba5f494ec07b3eaf42f7774671263cad7
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-04-05 16:25:38 -0400

    Don't resend "Now online" message

--- lua/discordrelay/events/ready.lua
+++ lua/discordrelay/events/ready.lua
@@ -1,12 +1,18 @@
+local Readied = false
+
 hook.Add("DiscordRelay::DispatchEvent", "DiscordRelay::Ready", function(Type, Message, Data)
 	if Type ~= "READY" then return end
 
 	DiscordRelay.Socket.SessionID = Message.session_id
 
-	DiscordRelay.Util.WebhookAutoSend({
-		["username"] = "Server Status",
-		["embeds"] = {
-			DiscordRelay.Util.CreateEmbed(Color(0, 255, 0), GetHostName(), "Server is now online!")
-		}
-	})
+	if not Readied then
+		DiscordRelay.Util.WebhookAutoSend({
+			["username"] = "Server Status",
+			["embeds"] = {
+				DiscordRelay.Util.CreateEmbed(Color(0, 255, 0), GetHostName(), "Server is now online!")
+			}
+		})
+
+		Readied = true
+	end
 end)
