c8b9ff7243aea990d9ffbd37420d1f855ed35a13
bjurd <177191469+bjurd@users.noreply.github.com>
2025-09-29 14:44:30 -0400

    Fix Role functions not being members
    Also add GetPermissionValue

--- lua/includes/modules/discord/objects/role.lua
+++ lua/includes/modules/discord/objects/role.lua
@@ -29,37 +29,43 @@ end
 
 --- Returns the snowflake ID
 --- @return string
-function GetID()
+function ROLE:GetID()
 	return self.Snowflake
 end
 
 --- Returns the display name
 --- @return string
-function GetName()
+function ROLE:GetName()
 	return self.Name
 end
 
 --- Returns the high and low permissions bits
 --- @return number, number
-function GetPermissions()
+function ROLE:GetPermissions()
 	return discord.bigint.Split(self.Permissions)
+end
+
+--- Returns the raw permissions bits
+--- @return string
+function ROLE:GetPermissionValue()
+	return self.Permissions
 end
 
 --- Returns the Color as a decimal
 --- @return number
-function GetColorDecimal()
+function ROLE:GetColorDecimal()
 	return self.Color
 end
 
 --- Returns the Color as a Garry's Mod Color
 --- @return Color
-function GetColor()
+function ROLE:GetColor()
 	return discord.colors.FromDecimal(self:GetColorDecimal())
 end
 
 --- Returns if the role is a managed role
 --- @return boolean
-function IsManaged()
+function ROLE:IsManaged()
 	return self.Managed
 end
 
