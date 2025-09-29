1133400cea3102d55eba676c1e4fbd98c4be8e3c
bjurd <177191469+bjurd@users.noreply.github.com>
2025-09-29 03:09:46 -0400

    Add broadcasted online message

--- lua/discordrelay/events/ready.lua
+++ lua/discordrelay/events/ready.lua
@@ -2,17 +2,12 @@ hook.Add("DiscordRelay::DispatchEvent", "SendOnlineMessage", function(Event, Soc
 	if Event ~= "READY" then return end
 
 	local Message = discord.messages.BeginMessage()
-		:WithContent("testing testing 123")
 		:WithEmbed()
-			:WithTitle("is this thing on?")
-			:WithDescription("heelllo world this is a super duper ultra crazy mega bonkers long piece of text because it's in the description it can be like that hurray")
-			:WithFooter("hahawhawhahawhawhawhawhawhawhaw")
-			:WithColor(color_white)
-			:End()
-		:WithEmbed()
-			:WithDescription("more than 1 (one)?")
-			:WithColorRGB(255, 0, 0)
+			:WithTitle(GetHostName())
+			:WithDescription("Server is now online!")
+			:WithFooter(Format("IP Address: %s", game.GetIPAddress()))
+			:WithColorRGB(0, 255, 0)
 			:End()
 
-	discord.messages.SendToChannel(Socket, "1139040735358881923", Message)
+	relay.conn.BroadcastMessage(Message)
 end)
