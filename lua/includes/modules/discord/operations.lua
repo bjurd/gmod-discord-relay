cf1ba0094bdd181b1ccec03b8783b236b38fed51
bjurd <177191469+bjurd@users.noreply.github.com>
2025-09-28 23:34:54 -0400

    Socket and Operation controllers

---
+++ lua/includes/modules/discord/operations.lua
@@ -0,0 +1,111 @@
+module("operations", package.discord)
+
+enums.CreateIncremental("OPERATION", {
+	"DISPATCH",
+	"HEARTBEAT",
+	"IDENTIFY",
+	"PRESENCE_UPDATE",
+	"VOICE_PING", -- No longer used as of API v5
+	"VOICE_UPDATE",
+	"RESUME",
+	"RECONNECT",
+	"REQUEST_MEMBERS",
+	"INVALID_SESSION",
+	"HELLO",
+	"HEARTBEAT_ACK"
+	-- Soundboard sounds jumps to 31 instead of 12 for some reason...
+})
+
+--- Manually (re-)fires an operation
+--- @param Socket WEBSOCKET A GWSockets socket instance
+--- @param Type number Operation type, see OPERATION_ enums
+--- @param Data table Operation data provided by the socket message
+function Fire(Socket, Type, Data)
+	logging.DevLog(LOG_NORMAL, "Firing operation %d", Type)
+
+	hook.Run("DiscordRelay::FireOperation", Type, Socket, Data)
+end
+
+--- Processes an operation by calling the related callback(s)
+--- @param Socket WEBSOCKET A GWSockets socket instance
+--- @param Type number Operation type, see OPERATION_ enums
+--- @param Data table Operation data provided by the socket message
+function Process(Socket, Type, Data)
+	Fire(Socket, Type, Data)
+end
+
+--- Creates a blank data packet for an operation
+--- @param Socket WEBSOCKET A GWSockets socket instance
+--- @param Type number Operation type, see OPERATION_ enums
+function CreateDataPacket(Socket, Type)
+	return {
+		["op"] = Type,
+
+		["d"] = {
+			["token"] = Socket.Token
+		}
+	}
+end
+
+--- Processes a dispatched event by calling the related callback(s)
+--- @param Socket WEBSOCKET A GWSockets socket instance
+--- @param Event string Event name, see https://discord.com/developers/docs/events/gateway-events
+--- @param Data table Event data, provided by the DISPATCH operation
+function DispatchEvent(Socket, Event, Data)
+	logging.DevLog(LOG_NORMAL, "Dispatching event %s", Event)
+
+	hook.Run("DiscordRelay::DispatchEvent", Event, Socket, Data)
+end
+
+--[[
+	Default operation/dispatch handlers
+--]]
+
+hook.Add("DiscordRelay::DispatchEvent", "DEFAULT::READY", function(Event, Socket, Data)
+	if Event ~= "READY" then return end
+
+	Socket.SessionID = Data.session_id
+	-- TODO: Resume
+
+	logging.DevLog(LOG_SUCCESS, "Socket is ready! Session ID: %s", Socket.SessionID)
+end)
+
+hook.Add("DiscordRelay::FireOperation", "DEFAULT::DISPATCH", function(Operation, Socket, Data)
+	if Operation ~= OPERATION_DISPATCH then return end
+
+	Socket.SequenceNumber = tonumber(Data.s) or 0
+
+	DispatchEvent(Socket, Data.t, Data.d)
+end)
+
+hook.Add("DiscordRelay::FireOperation", "DEFAULT::INVALID_SESSION", function(Operation, Socket, Data)
+	if Operation ~= OPERATION_INVALID_SESSION then return end
+
+	logging.Log(LOG_ERROR, "Socket has an invalid session!")
+
+	if Data.d == true then
+		-- TODO: Resume if possible
+	end
+end)
+
+hook.Add("DiscordRelay::FireOperation", "DEFAULT::HELLO", function(Operation, Socket, Data)
+	if Operation ~= OPERATION_HELLO then return end
+
+	logging.DevLog(LOG_NORMAL, "Sending IDENTIFY from HELLO")
+
+	local DataPacket = CreateDataPacket(Socket, OPERATION_IDENTIFY)
+
+	local PacketData = DataPacket.d
+	PacketData["intents"] = INTENTS_DEFAULT -- TODO: Read this from the config
+	PacketData["compress"] = false -- Discord uses zlib compression that Garry's Mod doesn't have a way to cope with
+
+	PacketData["properties"] = { -- This is required (or else you get INVALID_SESSION), so let's make it on mobile :D!
+		["os"] = "linux",
+		["browser"] = "Discord iOS",
+		["device"] = "Discord iOS"
+	}
+
+	socket.WriteData(Socket, DataPacket)
+
+	-- TODO: Heartbeat
+end)
---
+++ lua/includes/modules/discord/socket.lua
@@ -0,0 +1,139 @@
+if not util.IsBinaryModuleInstalled("gwsockets") then
+	logging.Log(LOG_WARNING, "GWSockets binary module is not installed! Sockets will be nonfunctional!")
+else
+	require("gwsockets")
+end
+
+module("socket", package.discord)
+
+SocketGatewayURL = "wss://gateway.discord.gg/?v=%d&encoding=json"
+
+--- @meta
+--- @alias WEBSOCKET table
+
+--- Callback for when the socket gets connected, internal use only
+--- @param self WEBSOCKET A GWSockets socket instance
+function SOCKET_OnConnected(self)
+	if not isstring(self.Token) then
+		logging.Log(LOG_ERROR, "Tried to connect socket with no token")
+		self:closeNow()
+
+		return
+	end
+
+	logging.Log(LOG_SUCCESS, "Socket connected to gateway")
+end
+
+--- Callback for when the socket gets disconnected, internal use only
+--- @param self WEBSOCKET A GWSockets socket instance
+function SOCKET_OnDisconnected(self)
+	logging.Log(LOG_WARNING, "Socket disconnected")
+end
+
+--- Callback for when the socket receives a message, internal use only
+--- @param self WEBSOCKET A GWSockets socket instance
+--- @param Message string The received message in JSON
+function SOCKET_OnMessage(self, Message)
+	Message = util.Decompress(Message) or Message
+
+	logging.DevLog(LOG_NORMAL, "Socket message: %s", Message)
+
+	local MessageData = util.JSONToTable(Message)
+
+	if not MessageData then
+		logging.DevLog(LOG_ERROR, "Socket got non-JSON message: %s", tostring(Message))
+		return
+	end
+
+	local Operation = tonumber(MessageData.op)
+
+	if not isnumber(Operation) then -- tonumber returns nil on failure
+		logging.DevLog(LOG_ERROR, "Socket got non-operational message: %s", tostring(Message))
+		return
+	end
+
+	operations.Process(self, Operation, MessageData)
+end
+
+--- Callback for when the socket receives an error message, internal use only
+--- @param self WEBSOCKET A GWSockets socket instance
+--- @param Message string The error message
+function SOCKET_OnError(self, Message)
+	logging.Log(LOG_ERROR, "Socket error: %s", Message)
+end
+
+--- Prepares a socket for communication with the gateway
+--- @param Socket WEBSOCKET A GWSockets socket instance
+function Prepare(Socket)
+	-- Socket:setMessageCompression(true)
+
+	Socket.onConnected = SOCKET_OnConnected
+	Socket.onDisconnected = SOCKET_OnDisconnected
+	Socket.onMessage = SOCKET_OnMessage
+	Socket.onError = SOCKET_OnError
+end
+
+--- Creates a socket to the Discord gateway
+--- @param API number The API version, see API_ enums
+--- @return WEBSOCKET|nil Socket A GWSockets socket instance, nil on failure
+function Create(API)
+	if not GWSockets then
+		logging.Log(LOG_ERROR, "Tried to create a socket without GWSockets!")
+		return nil
+	end
+
+	local Status = versioning.GetAPIStatus(API)
+
+	if Status == API_STATUS_UNKNOWN then
+		logging.Log(LOG_ERROR, "Tried to create a socket with an unknown API version!")
+		return nil
+	end
+
+	if Status == API_STATUS_DISCONTINUED then
+		logging.Log(LOG_ERROR, "Tried to create a socket with a discontinued API version!")
+		return nil
+	end
+
+	if Status == API_STATUS_DEPRECATED then
+		logging.Log(LOG_WARNING, "Creating a socket with deprecated API version %d", API)
+	end
+
+	local Socket = GWSockets.createWebSocket(Format(SocketGatewayURL, API))
+
+	if Socket then
+		logging.Log(LOG_SUCCESS, "Created a socket with API version %d", API)
+
+		Socket.API = API
+		Prepare(Socket)
+
+		return Socket
+	else
+		logging.Log(LOG_WARNING, "Failed to create a socket with API version %d", API)
+		return nil
+	end
+end
+
+--- Connects a socket to the Discord gateway
+--- @param Socket WEBSOCKET A GWSockets socket instance
+--- @param Token string The token to use for authentication
+function Connect(Socket, Token)
+	Socket.Token = Token
+
+	Socket:open()
+end
+
+--- Writes a data table by converting it to JSON
+--- @param Socket WEBSOCKET A GWSockets socket instance
+--- @param Data table The JSON data to send
+function WriteData(Socket, Data)
+	local JSON = util.TableToJSON(Data)
+
+	Socket:write(JSON)
+end
+
+--- Writes JSON to the socket
+--- @param Socket WEBSOCKET A GWSockets socket instance
+--- @param JSON string The JSON data to send
+function WriteJSON(Socket, JSON)
+	Socket:write(JSON)
+end
