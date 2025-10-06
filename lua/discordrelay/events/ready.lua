974c6f976cdc7cd4e8799de06c9059440fd73636
bjurd <177191469+bjurd@users.noreply.github.com>
2025-10-06 02:53:37 -0400

    Move Ready's footer to a var

--- lua/discordrelay/events/ready.lua
+++ lua/discordrelay/events/ready.lua
@@ -1,13 +1,15 @@
 hook.Add("DiscordRelay::DispatchEvent", "DEFAULT::SendOnlineMessage", function(Event, Socket)
 	if Event ~= "READY" then return end
 
+	local Footer = Format("IP Address: %s", game.GetIPAddress())
+
 	local Message = discord.messages.Begin()
 		:WithUsername("Server Status")
 		:WithEmbed()
 			:WithTitle(GetHostName())
 			:WithDescription("Server is now online!")
 			:WithFooter()
-				:WithText(Format("IP Address: %s", game.GetIPAddress()))
+				:WithText(Footer)
 				:End()
 			:WithColorRGB(0, 255, 0)
 			:End()
