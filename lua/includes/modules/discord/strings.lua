91008a555e197e1d8ecd2b2209c554b9a3db16d5
bjurd <177191469+bjurd@users.noreply.github.com>
2026-03-03 19:42:08 -0500

    Remove combination codes from names

--- lua/includes/modules/discord/strings.lua
+++ lua/includes/modules/discord/strings.lua
@@ -26,10 +26,39 @@ function SafeFormat(String, ...)
 	return String
 end
 
+--- @param Code number
+--- @return boolean
+function IsCombiningCode(Code)
+	if Code >= 0x0300 and Code <= 0x036F then return true end
+
+	if Code >= 0x0591 and Code <= 0x05BD then return true end
+	if Code == 0x05BF then return true end
+	if Code >= 0x05C1 and Code <= 0x05C2 then return true end
+	if Code >= 0x05C4 and Code <= 0x05C7 then return true end
+
+	if Code == 0x200B or Code == 0x200C or Code == 0x200D or Code == 0x2060 or (Code >= 0x200E and Code <= 0x200F) or (Code >= 0x202A and Code <= 0x202E) then
+		return true
+	end
+
+	return false
+end
+
 --- Cleans a username of invalid characters/sequences
 --- @param Username string
 --- @return string
 function CleanUsername(Username)
+	local Cleaned = {}
+
+	for _, Code in utf8.codes(Username) do
+		if Code >= 32 and Code ~= 127 then
+			if not IsCombiningCode(Code) then
+				table.insert(Cleaned, utf8.char(Code))
+			end
+		end
+	end
+
+	Username = table.concat(Cleaned)
+
 	Username = string.gsub(Username, "[@#:`]", "") -- Cannot contain @, #, :, ``` - This seems to be a complete lie because it still works with these names, nice docs!
 	Username = string.gsub(Username, "[dD][iI][sS][cC][oO][rR][dD]", "") -- Cannot contain "discord"
 	Username = string.gsub(Username, "%s+", " ") -- Collapse repetitive whitespace
